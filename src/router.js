841f1a55a88317bdd839851d1e44463e666b861b
Koalayusea <949177600@qq.com>
2019-09-21 19:41:27 +0800

    九宫格

--- src/router.js
+++ src/router.js
@@ -9,10 +9,11 @@ import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 // 3.创建路由对象
 var router = new VueRouter({
     routes: [//配置路由规则
-       { path: '/home', component: HomeContainer },
-       { path: '/member', component: MemberContainer },
-       { path: '/search', component: SearchContainer },
-       { path: '/shopcar', component: ShopcarContainer },
+        { path: '/', redirect: '/home' },
+        { path: '/home', component: HomeContainer },
+        { path: '/member', component: MemberContainer },
+        { path: '/search', component: SearchContainer },
+        { path: '/shopcar', component: ShopcarContainer },
     ],
     linkActiveClass: ' mui-active' //覆盖默认的路由高亮的类
 })
